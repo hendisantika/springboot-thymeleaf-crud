ece2ec429ff41717bb143256f97bb6ceae45bc89
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-23 09:01:35 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootthymeleafcrud/SpringbootThymeleafCrudApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootthymeleafcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootThymeleafCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootthymeleafcrud/SpringbootThymeleafCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootthymeleafcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
