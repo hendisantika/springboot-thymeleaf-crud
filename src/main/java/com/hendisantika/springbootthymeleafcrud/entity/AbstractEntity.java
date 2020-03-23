687e2f054a82b2129b1c0d17c5ff8f0f21b140c8
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-23 09:09:22 +0700

    #3 - Add AbstractEntity

---
+++ src/main/java/com/hendisantika/springbootthymeleafcrud/entity/AbstractEntity.java
@@ -0,0 +1,45 @@
+package com.hendisantika.springbootthymeleafcrud.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-thymeleaf-crud
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/03/20
+ * Time: 09.08
+ */
+@MappedSuperclass
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class AbstractEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime created;
+
+    @LastModifiedDate
+    private LocalDateTime updated;
+
+    private boolean deleted;
+
+}
