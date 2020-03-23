05c3b3a53a8b55bd2c057b02e602d938e1aff8b6
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-23 09:20:00 +0700

    #23 - Add view in ArticleController

--- src/main/java/com/hendisantika/springbootthymeleafcrud/controller/ArticleController.java
+++ src/main/java/com/hendisantika/springbootthymeleafcrud/controller/ArticleController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
@@ -34,6 +35,12 @@ public class ArticleController {
     public String search(@RequestParam(value = "term", required = false, defaultValue = "") String term, Model model) {
         model.addAttribute("articles", articleService.search(term));
         return "articles/index";
+    }
+
+    @GetMapping("/article/view/{slug}")
+    public String view(@PathVariable String slug, Model model) {
+        model.addAttribute("articleView", articleService.getArticle(slug));
+        return "articles/view";
     }
 
 }
