1f2527d19dd3b9b7d754b23eca498c74d62b8e91
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-02 06:05:54 +0700

    #49 - Add @EnableJpaAuditing

--- src/main/java/com/hendisantika/springbootthymeleafcrud/SpringbootThymeleafCrudApplication.java
+++ src/main/java/com/hendisantika/springbootthymeleafcrud/SpringbootThymeleafCrudApplication.java
@@ -2,8 +2,10 @@ package com.hendisantika.springbootthymeleafcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringbootThymeleafCrudApplication {
 
     public static void main(String[] args) {
